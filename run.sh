f662d687ff26bce564b7ffd8974940071475d291
[Sergey K.] <seriy.karp2@gmail.com>
2023-03-02 18:11:55 +0300

    minor changes

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ if [ ! -d "env" ]; then
     source env/bin/activate
     pip install -r requirements.txt
 else
+    echo "Env located, installing missing deps..."
     source env/bin/activate
+    pip install -r requirements.txt
 fi
 
 python3 main.py $1 $2 $3
