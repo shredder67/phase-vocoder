2ba8a9470266e0194b18ffb26410e1ab6eb2c44b
[Sergey K.] <seriy.karp2@gmail.com>
2023-03-02 17:34:10 +0300

    stftf tested and sped up by using fft

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python3 main.py --input_file $1 --output_file $2 --stratch_ratio $3
+if [ ! -d "env" ]; then
+    echo "Creating virtual environment and installing dependencies..."
+    python3 -m venv env
+    source env/bin/activate
+    pip install -r requirements.txt
+else
+    source env/bin/activate
+fi
+
+python3 main.py $1 $2 $3
